f49b97ab78557e77d1d0c167a3f280c4bf864129
devansh-dvj <devansh.dvj@gmail.com>
2022-11-29 04:26:42 +0530

    Updated scripts

--- summarize.py
+++ summarize.py
@@ -11,15 +11,14 @@ def extract(result_file: str):
     num_instructions = params[0][8:]
     model = params[1]
     trace_type = params[2]
-    trace_name = params[3]
+    trace_name = params[3][:-4]
 
     with open(result_file, 'r') as f:
         lines = f.readlines()
         l21 = lines[21].split()
         IPC = l21[4]
-        cycles = l21[8]
 
-    return num_instructions, model, trace_type, trace_name, IPC, cycles
+    return num_instructions, model, trace_type, trace_name, IPC
 
 
 if __name__ == '__main__':
@@ -28,18 +27,16 @@ if __name__ == '__main__':
         sys.exit(1)
 
     filename = sys.argv[1]
-    out_file = open("results.csv",'w')
+    out_file = open("results.csv", 'w')
 
     results = []
     with open(os.path.join(BASE_DIR, filename)) as f:
         for line in f:
             results.append(line.strip())
 
-
-    print(results)
     writer = csv.writer(out_file)
     print('Total results: {}'.format(len(results)))
+    writer.writerow(['#instr', 'model', 'trace_type', 'trace_name', 'IPC'])
     for row in results:
         writer.writerow(extract(row))
-    print(extract(results[0]))
     out_file.close
